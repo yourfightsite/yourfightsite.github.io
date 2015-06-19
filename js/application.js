f494747dfa51e0a542fdfba6080f387e8ad608ad
Martin Bean <martin@martinbean.co.uk>
2015-06-19 11:10:38 +0100

    Home jumbotron

--- js/application.js
+++ js/application.js
@@ -1,5 +1,9 @@
 ;(function ($) {
 
-  //
+  // Open external links in a new window
+  $('a[rel="external"]').on('click', function (e) {
+    e.preventDefault();
+    window.open(this.href);
+  });
 
 })(jQuery);
